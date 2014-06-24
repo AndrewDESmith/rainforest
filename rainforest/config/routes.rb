b6a96a03dbca6f4999ac876c3732bc38633ee785
Andrew Smith <andrew.d.e.smith@gmail.com>
2014-06-24 17:24:52 -0400

    Creating a user model, controller, and view

--- rainforest/config/routes.rb
+++ rainforest/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   get 'products/index'
 
   get 'products/show'
@@ -8,6 +12,7 @@ Rails.application.routes.draw do
   get 'products/edit'
 
   resources :products
+  resources :users, :only => [:new, :create]
   root "products#index"
 
   # get "products" => "products#index"
